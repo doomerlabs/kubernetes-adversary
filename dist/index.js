ca921aee6509f3ca118a831d8e5694946fe62f8a
adversarylabs-release <adversarylabs-release@users.noreply.github.com>
2026-09-22 19:12:50 +0000

    chore: bump doomer version to 0.0.16 [skip-ci]

--- dist/index.js
+++ dist/index.js
@@ -18466,7 +18466,7 @@ function matchesGlob(path, glob) {
 
 // src/index.ts
 function createApp() {
-  const app = new Adversary({ name: "kubernetes", version: "0.0.15", review: { maximumFindings: 8 } });
+  const app = new Adversary({ name: "kubernetes", version: "0.0.16", review: { maximumFindings: 8 } });
   registerRules(app);
   app.rule("kubernetes.review", async (ctx) => analyzeRepository(ctx));
   return app;
